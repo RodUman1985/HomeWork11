76fd7d6526e5ace9c07ac6ef85be42a2733ec9bc
RodUman1985 <Vand_85@mail.ru>
2022-12-08 12:55:52 +0300

    2

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 async function loadCurrecies(){
     const res= await fetch('https://date.nager.at/api/v3/AvailableCountries');
     console.log (res);
-     let currensies= await res.json();
+     let avCon= await res.json();
      
-     drawCurrencies(currensies);
+     drawCurrencies(avCon);
      
 }
 loadCurrecies();
-function drawCurrencies(currensies){
-    const select = document.getElementById('currencies');
-    currensies.forEach((c) => {
+function drawCurrencies(avCon){
+    const select = document.getElementById('avCon');
+    avCon.forEach((c) => {
         const option = document.createElement('option');
         option.innerText=c.Name;
         option.value=c.countryCode;
@@ -18,3 +18,30 @@ function drawCurrencies(currensies){
 
 }
 
+async function showExRate (id){
+    const url=`https://www.nbrb.by/api/exrates/rates/${id}`;
+    const res = await fetch(url);
+    const rate= await res.json();
+    const d= document.getElementById('exrate');
+    d.innerText=`${rate.Cur_Scale}${rate.Cur_Name} = ${rate.Cur_OfficialRate} BYN`;
+}
+async function sendMessage(){
+    const username=document.getElementById('username');
+    const msg=document.getElementById('msg');
+    const req= {
+        name:username.value,
+        messege: msg.value
+
+    }
+    const reqJSON= JSON.stringify(req);
+    const url = 'http://192.168.100.101:8080';
+   await fetch(url,{
+        method:'POST',
+        body: reqJSON,
+        headers:{
+            'Content-Type':'application/json'
+        }
+
+    })
+    msg.value='';
+}
